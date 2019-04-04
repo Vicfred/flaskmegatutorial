442de81d27e8f391d75971a32fb0896b9b0c1110
Vicfred <vicfredboy@gmail.com>
2019-04-03 18:54:13 -0600

    last seen

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,9 +1,17 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+from datetime import datetime
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/')
